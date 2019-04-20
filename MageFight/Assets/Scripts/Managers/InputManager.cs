db7e1bfdb7db21d0cb6c34ae6ac89eeff78e0637
Unknown <dani_mrx93@hotmail.com>
2019-04-20 18:57:22 -0300

    Add InputManager and edit inpu on project settings

---
+++ MageFight/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    #region Input
+    public string jumpButton; // R1 (Bumper)
+    public string blockButton; // X / A Button?
+    public string movementAxisY; // L Stick
+    public string movementAxisX; // L Stick
+    public string aimAxisY; // R Stick
+    public string aimAxisX; // R Stick
+    public string firstSkillButton; // L1 (Bumper)
+    public string secondSkillButton; // L2 (Trigger)
+    public string thirdSkillButton; // R2 (Trigger)
+    public string pauseButton; // Start
+    #endregion
+}
