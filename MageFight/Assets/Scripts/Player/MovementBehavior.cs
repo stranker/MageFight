9df6c19229abb37ca0d84c65ce37081765e7a5a3
Akirame <gsvillalba12@hotmail.com>
2019-05-12 21:30:05 -0300

    Add Dash behavior

--- MageFight/Assets/Scripts/Player/MovementBehavior.cs
+++ MageFight/Assets/Scripts/Player/MovementBehavior.cs
@@ -19,11 +19,18 @@ public class MovementBehavior : MonoBehaviour {
     private bool knockback = false;
     public float timer;
     public float knockbackTime = 1;
-
+    private Vector2 aimDirection;
+    private bool dashing;
+    public float dashSpeed;
+    public float dashTimer;
+    public float dashTotalTime = 2f;
+    private float gravity;
     // Use this for initialization
     void Start () {
         rd = GetComponent<Rigidbody2D>();
         input = GetComponent<InputManager>();
+        dashTimer = 0;
+        gravity = rd.gravityScale;
 	}
 	
     private void FixedUpdate()
@@ -39,17 +46,47 @@ public class MovementBehavior : MonoBehaviour {
         else
             velocity.x = 0;
 
-        if (Input.GetButtonDown(input.jumpButton) && onFloor)
+        if (Input.GetButtonDown(input.jumpButton) && onFloor && !dashing)
         {
             onFloor = !onFloor;
             rd.velocity = new Vector2(0,jumpForce);
+        }
+        if(Input.GetButtonDown(input.dodgeButton) || dashing)
+        {
+            canMove = false;
+            if(!dashing)
+            {
+                int upDir = (int)Input.GetAxis(input.aimAxisY);
+                int fwDir = Mathf.Abs(Input.GetAxis(input.movementAxisX)) > 0.1f ? (int)transform.localScale.x : 0;
+                aimDirection = new Vector2(fwDir == 0 && upDir == 0 ? transform.localScale.x : fwDir, upDir);
+            }
+            Dash(aimDirection);
+        }
+    }
+
+    private void Dash(Vector2 dir)
+    {
+        dashing = true;
+        if(dashTimer < dashTotalTime)
+        {
+            rd.gravityScale = 0;
+            rd.velocity = Vector2.zero;
+            transform.position += new Vector3(dir.x * dashSpeed, dir.y * dashSpeed);
+            dashTimer += Time.deltaTime;
+        }
+        else
+        {
+            rd.gravityScale = gravity;
+            dashTimer = 0;
+            dashing = false;
+            canMove = true;
         }
     }
 
     private void Movement()
     {
-        if (!knockback)
-        {
+        if (!knockback && !dashing)
+        {            
             if (onFloor)
                 rd.velocity = new Vector2(velocity.x * floorSpeed * Time.deltaTime, rd.velocity.y);
             else
