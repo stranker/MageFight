550b67800af3d1d743c11daf2420aa00cd1995d6
Unknown <tmugetti@gmail.com>
2019-05-22 00:11:38 -0300

    "Main menu" button bug fixed
    Scene indexes were not reversed, or otherwise changed from last commit.

---
+++ MageFight/Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour {
+
+	public void LoadMainMenu(){
+		LoaderManager.Get().LoadScene("MainMenu");
+	}
+}
