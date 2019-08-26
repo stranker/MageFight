35d743f2eee940c1f107260648f8dc070f3dc131
Daniel <danielnatarelli@gmail.com>
2019-08-26 05:16:39 -0300

    Fix melee spells bug that don't show sprite correctly on invoked

--- MageFight/Assets/Scripts/Spells/MeleeSpell.cs
+++ MageFight/Assets/Scripts/Spells/MeleeSpell.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -37,12 +38,21 @@ public class MeleeSpell : Spell {
         transform.position = startPos;
         canHit = true;
         dir = direction;
+        CheckAimDirection();
         timer = cooldown;
         transform.localScale = new Vector2(dir.x, 1);
         SetAnimation();
     }
-	
-	private void SetAnimation()
+
+    private void CheckAimDirection()
+    {
+        if (dir.x == 0)
+        {
+            dir.x = mageOwner.GetComponent<PlayerMovement>().currentDirection;
+        }
+    }
+
+    private void SetAnimation()
 	{
         GetComponent<Animator>().SetTrigger(spellName);
     }
