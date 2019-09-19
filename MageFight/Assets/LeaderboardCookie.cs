f19f52a0facef8998e51c4e52da1edb02609006e
Unknown <dani_mrx93@hotmail.com>
2019-09-19 05:28:47 -0300

    Hotfix
    Fix missing references and new leaderboard UI

---
+++ MageFight/Assets/LeaderboardCookie.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardCookie : MonoBehaviour
+{
+
+    public Sprite empty;
+    public Sprite cookie;
+    public Image image;
+
+    public void ChangeToCookie()
+    {
+        image.sprite = cookie;
+    }
+
+}
