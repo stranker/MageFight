5d56fd04eec33d345088aabc24b669d2e7e25ea3
Unknown <dani_mrx93@hotmail.com>
2019-10-02 06:19:22 -0300

    Add Character selection screen

---
+++ MageFight/Assets/CharacterSelectionDisplay.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectionDisplay : MonoBehaviour
+{
+
+    public List<CharacterSelection> characterDataList;
+    public int characterIdx = 0;
+
+    private CharacterSelection characterData;
+
+    public Text characterName;
+    public Text characterDesc;
+    public Image characterArtwork;
+    public Image leftArrow;
+    public Image rightArrow;
+    public Image inputImage;
+
+    public int playerId;
+
+    public CharacterSelectionManager.InputType inputType;
+    public Sprite[] inputImageList;
+
+
+    public GameObject initPanel;
+    public GameObject playerPanel;
+
+    public bool playerConfirmed = false;
+
+    private float canConfirmTimer;
+    private float canConfirmTime = 1.1f;
+    private bool canConfirm = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftArrow.gameObject.SetActive(false);
+        SelectCharacterAt(characterIdx);
+    }
+
+    private void Update()
+    {
+        if (!canConfirm && playerPanel.activeInHierarchy)
+        {
+            if (canConfirmTimer > 0)
+            {
+                canConfirmTimer -= Time.deltaTime;
+            }
+            else
+            {
+                canConfirm = true;
+                canConfirmTimer = 0;
+            }
+        }
+
+    }
+
+    private void SelectCharacterAt(int idx)
+    {
+        characterIdx = Mathf.Clamp(idx, 0, characterDataList.Count - 1);
+        characterData = characterDataList[characterIdx];
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        leftArrow.gameObject.SetActive(characterIdx != 0);
+        rightArrow.gameObject.SetActive(characterIdx != characterDataList.Count - 1);
+        characterName.text = characterData.characterName;
+        characterDesc.text = characterData.description;
+        characterArtwork.sprite = characterData.artwork;
+        inputImage.sprite = inputImageList[(ushort)inputType];
+    }
+
+    public void Initialize(int currentPlayerId, CharacterSelectionManager.InputType inputType)
+    {
+        playerId = currentPlayerId;
+        this.inputType = inputType;
+        initPanel.SetActive(false);
+        playerPanel.SetActive(true);
+        canConfirmTimer = canConfirmTime;
+        UpdateUI();
+    }
+
+    private void OnGUI()
+    {
+        switch (inputType)
+        {
+            case CharacterSelectionManager.InputType.Joystick:
+                CheckJoystickInput();
+                break;
+            case CharacterSelectionManager.InputType.Keyboard:
+                CheckKeyboardInput();
+                break;
+            case CharacterSelectionManager.InputType.Count:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void CheckJoystickInput()
+    {
+        if (!playerConfirmed)
+        {
+            if (Input.GetKey(KeyCode.JoystickButton1))
+            {
+                characterIdx++;
+                SelectCharacterAt(characterIdx);
+            }
+            if (Input.GetKey(KeyCode.JoystickButton2))
+            {
+                characterIdx--;
+                SelectCharacterAt(characterIdx);
+            }
+            if (Input.GetKey(KeyCode.JoystickButton0) && canConfirm && !playerConfirmed)
+            {
+                playerConfirmed = true;
+                characterName.text = "READY!";
+                CharacterSelectionManager.Instance.AddPlayerConfirm();
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.JoystickButton3) && playerConfirmed)
+            {
+                playerConfirmed = false;
+                CharacterSelectionManager.Instance.RemovePlayerConfirm();
+                UpdateUI();
+            }
+        }
+    }
+
+    private void CheckKeyboardInput()
+    {
+        if (!playerConfirmed)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                characterIdx++;
+                SelectCharacterAt(characterIdx);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                characterIdx--;
+                SelectCharacterAt(characterIdx);
+            }
+            if (Input.GetKeyDown(KeyCode.Return) && canConfirm && !playerConfirmed)
+            {
+                playerConfirmed = true;
+                characterName.text = "READY!";
+                CharacterSelectionManager.Instance.AddPlayerConfirm();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && playerConfirmed)
+            {
+                playerConfirmed = false;
+                CharacterSelectionManager.Instance.RemovePlayerConfirm();
+                UpdateUI();
+            }
+        }
+
+    }
+}
---
+++ MageFight/Assets/CharacterSelectionManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectionManager : MonoBehaviour
+{
+
+    private static CharacterSelectionManager instance;
+    public static CharacterSelectionManager Instance
+    {
+        get
+        {
+            instance = FindObjectOfType<CharacterSelectionManager>();
+            if (instance == null)
+            {
+                GameObject go = new GameObject("CharacterSelectionManager");
+                instance = go.AddComponent<CharacterSelectionManager>();
+            }
+            return instance;
+
+        }
+    }
+
+    public enum InputType {Joystick = 0, Keyboard, Count }
+    public Dictionary<int, InputType> players = new Dictionary<int, InputType>();
+    public Dictionary<int, string> joystickList = new Dictionary<int, string>();
+    public int currentPlayerId = 0;
+    private int joystickIdx = 0;
+    public int maxPlayers;
+    public CharacterSelectionDisplay[] characterSelectionDisplays;
+    public int playersConfirmed = 0;
+
+    public GameObject startUI;
+    private float canStartTimer = 0;
+    private float canStartTime = 1;
+    public bool canStart = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        characterSelectionDisplays = GetComponentsInChildren<CharacterSelectionDisplay>();
+        maxPlayers = characterSelectionDisplays.Length;
+    }
+
+    private void Update()
+    {
+        CheckGamepadInput();
+        CheckKeyboardInput();
+        CheckPlayersConfirmedCount();
+    }
+
+    private void CheckPlayersConfirmedCount()
+    {
+        startUI.SetActive(playersConfirmed > 1);
+        if (canStart)
+        {
+            canStartTimer += Time.deltaTime;
+        }
+        if ((Input.GetKeyDown(KeyCode.Return) || Input.GetKey(KeyCode.JoystickButton10)) && canStartTimer >= canStartTime)
+        {
+            GoToNextScreen();
+        }
+    }
+
+    private void GoToNextScreen()
+    {
+        print("Go to map selection");
+    }
+
+    private void CheckGamepadInput()
+    {
+        if (Input.GetKey(KeyCode.JoystickButton0))
+        {
+            if (currentPlayerId < maxPlayers)
+            {
+                if (joystickIdx < Input.GetJoystickNames().Length && !joystickList.ContainsValue(Input.GetJoystickNames()[joystickIdx]))
+                {
+                    players.Add(currentPlayerId, InputType.Joystick);
+                    joystickList.Add(currentPlayerId, Input.GetJoystickNames()[joystickIdx]);
+                    joystickIdx++;
+                    characterSelectionDisplays[currentPlayerId].Initialize(currentPlayerId, players[currentPlayerId]);
+                    currentPlayerId++;
+                }
+            }
+        }
+    }
+
+    private void CheckKeyboardInput()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            if (!players.ContainsValue(InputType.Keyboard) && currentPlayerId < maxPlayers)
+            {
+                players.Add(currentPlayerId, InputType.Keyboard);
+                characterSelectionDisplays[currentPlayerId].Initialize(currentPlayerId, players[currentPlayerId]);
+                currentPlayerId++;
+            }
+        }
+    }
+
+    public void AddPlayerConfirm()
+    {
+        playersConfirmed++;
+        canStart = playersConfirmed > 1;
+    }
+
+    public void RemovePlayerConfirm()
+    {
+        playersConfirmed--;
+        canStart = false;
+        canStartTimer = 0;
+    }
+}
